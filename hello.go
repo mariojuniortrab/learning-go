19b8e65adde4e60f3c190f3e58ec63c51681b995
Mário Júnior <mariojuniortrab@gmail.com>
2022-09-19 11:30:09 -0300

    variables

--- hello.go
+++ hello.go
@@ -7,6 +7,6 @@ func main() {
 	var versao float32 = 1.1
 	var idade int = 32
 
-	fmt.Println("Olá, sr.", nome, "sua idade é ", idade)
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão: ", versao)
 }
