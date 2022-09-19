22bf51090c79a1d6c2a7d527b148b1da8f142d85
Mário Júnior <mariojuniortrab@gmail.com>
2022-09-19 11:28:55 -0300

    variables

--- hello.go
+++ hello.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá Mundo")
+	var nome string = "Mario"
+	var versao float32 = 1.1
+	var idade int = 32
+
+	fmt.Println("Olá, sr.", nome, "sua idade é ", idade)
+	fmt.Println("Este programa está na versão: ", versao)
 }
