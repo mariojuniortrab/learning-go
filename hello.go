c7ca9ee62d5dddcbe460ebf13ffac581110d42ff
Mário Júnior <mariojuniortrab@gmail.com>
2022-09-19 16:46:02 -0300

    crating functions

--- hello.go
+++ hello.go
@@ -1,32 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		iniciarMonitoramento()
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do Programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+
+}
+
+func exibeIntroducao() {
 	nome := "Mario"
 	versao := 1.1
 	idade := 32
 
 	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão: ", versao)
+}
 
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi: ", comandoLido)
+
+	return comandoLido
+}
+
+func exibeMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi: ", comando)
-
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do Programa...")
-	default:
-		fmt.Println("Não conheço este comando")
-	}
-
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
 }
