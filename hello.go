b8acdf08cfa4024ad8181f6f3952b3ddbb10724c
Mário Júnior <mariojuniortrab@gmail.com>
2022-09-19 11:49:58 -0300

    defining variables

--- hello.go
+++ hello.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var nome string = "Mario"
-	var versao float32 = 1.1
-	var idade int = 32
+	nome := "Mario"
+	versao := 1.1
+	idade := 32
 
 	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa está na versão: ", versao)
+
+	fmt.Println("O tipo da variável nome é:", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variável idade é:", reflect.TypeOf(idade))
+	fmt.Println("O tipo da variável versao é:", reflect.TypeOf(versao))
 }
